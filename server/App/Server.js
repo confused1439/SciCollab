881e51b10ab5d4e795316ec1d707bf0059b430c0
Vivek Saradva <129260694+confused1439@users.noreply.github.com>
2024-07-20 18:11:30 +0530

    Update Server.js

--- server/App/Server.js
+++ server/App/Server.js
@@ -2,11 +2,20 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const cors = require('cors')
 const mongoose = require("mongoose");
 const app = express();
 const route = require("../Router/routes");
 
 app.use(express.json());
+
+app.use(cors(
+  {
+    origin: ["https://sci-collab-pi.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+))
 
 app.use(
   session({
